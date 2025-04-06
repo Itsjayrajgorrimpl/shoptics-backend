4b4fb7b16939f6f0cf1c6dda14da9957f82675bc
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2025-04-06 16:18:36 +0530

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -1,10 +1,25 @@
-
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'buyer' // or 'seller', 'admin'
+  }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
