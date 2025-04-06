4e1c477fc2584dfea8c12588dc38e6d32351c1fc
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2025-04-06 15:43:03 +0530

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,13 @@
+
+const User = require('../models/userModel');
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
