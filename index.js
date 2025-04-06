0cf516ea539171633f12c296c9eb1e76eb2309f0
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2025-04-06 15:22:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('DB Error:', err));
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Shoptics backend is working!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
